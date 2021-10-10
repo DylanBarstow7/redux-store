589b7f4c4edc6939843ea13853027648577586f1
Dylan Barstow <dcbarstow7@gmail.com>
2021-10-09 19:41:12 -0500

    updated initial state and added comments

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,4 +1,4 @@
-import { useReducer } from 'react';
+
 import {
   UPDATE_PRODUCTS,
   ADD_TO_CART,
@@ -11,31 +11,35 @@ import {
   TOGGLE_CART,
 } from './actions';
 
-// TODO: To get a better understand of how a reducer works - add comments to the various actions in the reducer
-export const reducer = (state, action) => {
+const initialState = {
+  products: [],
+  categories: [],
+  currentCategory: '',
+  cart: [],
+  cartOpen: false
+};
+
+export const reducer = (state = initialState, action) => {
   switch (action.type) {
-    // TODO: Add a comment describing the functionality of the UPDATE_PRODUCTS case
-    // Your comment here
+    // Returns a copy of state with an update products array. We use the action.products property and spread it's contents into the new array.
     case UPDATE_PRODUCTS:
       return {
         ...state,
         products: [...action.products],
       };
-
     case ADD_TO_CART:
       return {
         ...state,
         cartOpen: true,
         cart: [...state.cart, action.product],
       };
-
     case ADD_MULTIPLE_TO_CART:
       return {
         ...state,
         cart: [...state.cart, ...action.products],
       };
-    // TODO: Add a comment describing the functionality of the UPDATE_CART_QUANTITY case
-    // Your comment here
+    // Returns a copy of state, sets the cartOpen to true and maps through the items in the cart.
+    // If the item's `id` matches the `id` that was provided in the action.payload, we update the purchase quantity.
     case UPDATE_CART_QUANTITY:
       return {
         ...state,
@@ -47,52 +51,46 @@ export const reducer = (state, action) => {
           return product;
         }),
       };
-
-    // TODO: Add a comment describing the functionality of the REMOVE_FROM_CART case
-    // Your comment here
+    // First we iterate through each item in the cart and check to see if the `product._id` matches the `action._id`
+    // If so, we remove it from our cart and set the updated state to a variable called `newState`
     case REMOVE_FROM_CART:
       let newState = state.cart.filter((product) => {
         return product._id !== action._id;
       });
-
+      // Then we return a copy of state and check to see if the cart is empty.
+      // If not, we set the cartOpen status to  `true`. Then we return an updated cart array set to the value of `newState`.
       return {
         ...state,
         cartOpen: newState.length > 0,
         cart: newState,
       };
-
     case CLEAR_CART:
       return {
         ...state,
         cartOpen: false,
         cart: [],
       };
-
     case TOGGLE_CART:
       return {
         ...state,
         cartOpen: !state.cartOpen,
       };
-
     case UPDATE_CATEGORIES:
       return {
         ...state,
         categories: [...action.categories],
       };
-
     case UPDATE_CURRENT_CATEGORY:
       return {
         ...state,
         currentCategory: action.currentCategory,
       };
 
-    // TODO: Add a comment describing what the default case is for
-    // Your comment here
+    // Return the state as is in the event that the `action.type` passed to our reducer was not accounted for by the developers
+    // This saves us from crashing.
     default:
       return state;
   }
 };
 
-export function useProductReducer(initialState) {
-  return useReducer(reducer, initialState);
-}
+export default reducer;
