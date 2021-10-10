bd9281c4ab4a9d480019d79ad79dbd2194e557f8
Dylan Barstow <dcbarstow7@gmail.com>
2021-10-09 19:45:04 -0500

    importing useDispatch and useSelector components

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,21 +1,18 @@
 import React from 'react';
-import { useStoreContext } from "../../utils/GlobalState";
+import { useDispatch } from 'react-redux';
 import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
 
 const CartItem = ({ item }) => {
 
-  const [, dispatch] = useStoreContext();
-
+  const dispatch = useDispatch();
   const removeFromCart = item => {
     dispatch({
       type: REMOVE_FROM_CART,
       _id: item._id
     });
     idbPromise('cart', 'delete', { ...item });
-
   };
-
   const onChange = (e) => {
     const value = e.target.value;
     if (value === '0') {
@@ -24,7 +21,6 @@ const CartItem = ({ item }) => {
         _id: item._id
       });
       idbPromise('cart', 'delete', { ...item });
-
     } else {
       dispatch({
         type: UPDATE_CART_QUANTITY,
@@ -32,10 +28,8 @@ const CartItem = ({ item }) => {
         purchaseQuantity: parseInt(value)
       });
       idbPromise('cart', 'put', { ...item, purchaseQuantity: parseInt(value) });
-
     }
   }
-
   return (
     <div className="flex-row">
       <div>
--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import ProductItem from '../ProductItem';
-import { useStoreContext } from '../../utils/GlobalState';
+import { useDispatch, useSelector } from 'react-redux';
 import { UPDATE_PRODUCTS } from '../../utils/actions';
 import { useQuery } from '@apollo/client';
 import { QUERY_PRODUCTS } from '../../utils/queries';
@@ -8,10 +8,10 @@ import { idbPromise } from '../../utils/helpers';
 import spinner from '../../assets/spinner.gif';
 
 function ProductList() {
-  const [state, dispatch] = useStoreContext();
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state);
 
   const { currentCategory } = state;
-
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
   useEffect(() => {
@@ -32,12 +32,10 @@ function ProductList() {
       });
     }
   }, [data, loading, dispatch]);
-
   function filterProducts() {
     if (!currentCategory) {
       return state.products;
     }
-
     return state.products.filter(
       (product) => product.category._id === currentCategory
     );
