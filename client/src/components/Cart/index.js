d38491a1d3467209bc68a9b7a79b14e54b68faef
Dylan Barstow <dcbarstow7@gmail.com>
2021-10-09 19:41:26 -0500

    importing useDispatch

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -5,14 +5,15 @@ import { QUERY_CHECKOUT } from '../../utils/queries';
 import { idbPromise } from '../../utils/helpers';
 import CartItem from '../CartItem';
 import Auth from '../../utils/auth';
-import { useStoreContext } from '../../utils/GlobalState';
+import { useDispatch, useSelector } from 'react-redux';
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
 import './style.css';
 
 const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Cart = () => {
-  const [state, dispatch] = useStoreContext();
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state);
   const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
 
   useEffect(() => {
@@ -28,16 +29,13 @@ const Cart = () => {
       const cart = await idbPromise('cart', 'get');
       dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
     }
-
     if (!state.cart.length) {
       getCart();
     }
   }, [state.cart.length, dispatch]);
-
   function toggleCart() {
     dispatch({ type: TOGGLE_CART });
   }
-
   function calculateTotal() {
     let sum = 0;
     state.cart.forEach((item) => {
@@ -45,21 +43,17 @@ const Cart = () => {
     });
     return sum.toFixed(2);
   }
-
   function submitCheckout() {
     const productIds = [];
-
     state.cart.forEach((item) => {
       for (let i = 0; i < item.purchaseQuantity; i++) {
         productIds.push(item._id);
       }
     });
-
     getCheckout({
       variables: { products: productIds },
     });
   }
-
   if (!state.cartOpen) {
     return (
       <div className="cart-closed" onClick={toggleCart}>
@@ -69,7 +63,6 @@ const Cart = () => {
       </div>
     );
   }
-
   return (
     <div className="cart">
       <div className="close" onClick={toggleCart}>
@@ -81,10 +74,8 @@ const Cart = () => {
           {state.cart.map((item) => (
             <CartItem key={item._id} item={item} />
           ))}
-
           <div className="flex-row space-between">
             <strong>Total: ${calculateTotal()}</strong>
-
             {Auth.loggedIn() ? (
               <button onClick={submitCheckout}>Checkout</button>
             ) : (
